1164ee593df963d14741e0f3b729e6c620e41b7a
Robert Arcaleanu <alumnerobert@gmail.com>
2024-04-28 16:52:38 +0200

    Downloaded all the data from AENA Website

--- data_load/extract.py
+++ data_load/extract.py
@@ -1,41 +1,50 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
 
 import conf
+import time
 
 # Replace with your desired URL
 URL = "https://www.aena.es/es/estadisticas/informes-mensuales.html?anio="
-years_range = list(range(2014, 2024))
+years_range = list(range(2004, 2025))
 
-download_directory = "C:/Users/rober/OneDrive/Escriptori/DataSets/Time-Series/time-series/data"
-chrome_options = Options()
+chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("prefs", {
-    "download.default_directory": download_directory
-})
+    "download.default_directory": "C:\\Users\\rober\\OneDrive\\Escriptori\\DataSets\\Time-Series\\time-series\\data"}    )
 
 # Initialize the webdriver (Chrome in this case)
-driver = webdriver.Chrome(executable_path=conf.CHROME_DRIVER_PATH)
+driver = webdriver.Chrome(executable_path=conf.CHROME_DRIVER_PATH, chrome_options=chrome_options)
 
-# Open the URL
+# maximize the window
+driver.maximize_window()
+
+i = 0
 for year in years_range:
     url = f"{URL}{year}"
     driver.get(url)
+    time.sleep(2)
 
-#  List all elements (might be overwhelming for complex pages)
-# all_elements = driver.find_elements_by_tag_name("*")
-# for element in all_elements:
-#     print(element.tag_name)
+    if i == 0:
+        # click on accept cookies
+        accept_cookies = driver.find_element(By.CSS_SELECTOR, "#modal_footer > div > div > div > button.ecix-bold.ecix-font-1-2em.elix-accordion-btn-primary")
+        accept_cookies.click()
+    
+    list_reports = driver.find_elements(By.CSS_SELECTOR, "div[class='informes']")
+    for report in list_reports:
+        download_report = report.find_element(By.CSS_SELECTOR, "div[class='descargas']")
+        download_report = download_report.find_elements(By.CSS_SELECTOR, "div[class='btn-download-pdf']")[1]
+        
+        # Adjust window to click on the download
+        y_coordinate = download_report.location['y']
+        adjusted_y_coordinate = y_coordinate - 500 
+        driver.execute_script("window.scrollTo(0, arguments[0]);", adjusted_y_coordinate)
 
-# Define how to identify the element you want to click on
-# Here we use By.ID, replace with your appropriate strategy (By.CLASS_NAME, By.XPATH etc.)
-element_to_click = driver.find_element(By.ID, "specific_element_id")  # Replace with actual ID
-
-# Click on the element
-element_to_click.click()
-
-# Do something after clicking (optional)
-#  ... your code here ...
+        time.sleep(1)
+        download_report.click()
+        
+    print(f"Year {year} downloaded")
+    time.sleep(2)
+    i = i +1
 
 # Close the browser (optional)
-# driver.quit()
+driver.quit()
---
+++ data_load/transform.py
@@ -0,0 +1,8 @@
+import os
+import polars as pl
+
+# Rename files with month and year
+
+
+# Combine all files into a dataframe
+
